f2efbb1b40fe0e6feb6110bdccfeffa153426732
nidhikumari <kumari9936nidhi@gmail.com>
2022-10-07 18:26:46 +0530

    added projects in data

---
+++ pages/data.js
@@ -0,0 +1,14 @@
+export const projectsObj = [
+    {
+        id: 1,
+        title: "My Travel Journal",
+        preview: "My Travel Journal Preview",
+        link: "https://n-fernweh.netlify.app"
+    },
+    {
+        id: 2,
+        title: "Meme Generator",
+        preview: "Meme Generator Preview",
+        link: "https://nmemegenerator.netlify.app"
+    },
+]
